57b8cffb64a9d18b42aeae2dd0b22069c8301c95
심이한 <ihanshim@sim-ihan-ui-MacBookAir.local>
2026-07-12 16:53:26 +0900

    feat: add detailed descriptions for all aircraft models

--- data/aircrafts.js
+++ data/aircrafts.js
@@ -5,6 +5,7 @@ const aircraftData = [
     {
         id: "a220-family",
         photoVariant: "A220-300",
+        description: "캐나다 봄바디어 C시리즈를 에어버스가 인수하여 재탄생시킨 차세대 고효율 단거리용 소형 광동체급 협동체 항공기입니다. 소음이 매우 적고 연료 효율성이 높으며 넓은 좌석 폭을 자랑합니다.",
         imageUrl: "images/A220-300.jpg",
         manufacturer: "Airbus",
         familyName: "A220 Family",
@@ -27,6 +28,7 @@ const aircraftData = [
     {
         id: "a300-family",
         photoVariant: "A300-600R",
+        description: "에어버스가 개발한 역사상 최초의 쌍발 광동체 여객기이자 에어버스의 기틀을 다진 역사적인 기종입니다. 중단거리 고수요 노선에서 큰 성공을 거두었으며 화물기로도 널리 쓰였습니다.",
         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Iran_Air_Airbus_A300-605R%3B_EP-IBD%40FRA%3B06.07.2011_603ks_%285915220574%29.jpg/500px-Iran_Air_Airbus_A300-605R%3B_EP-IBD%40FRA%3B06.07.2011_603ks_%285915220574%29.jpg",
         manufacturer: "Airbus",
         familyName: "A300 Family",
@@ -52,6 +54,7 @@ const aircraftData = [
     {
         id: "a310-family",
         photoVariant: "A310",
+        description: "A300을 기반으로 동체를 단축하고 항속거리를 늘린 쌍발 광동체기입니다. 에어버스의 중형 광동체 라인업을 보강하며 대서양 횡단 노선 등에서 주로 활약했습니다.",
         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Air_Transat_A310_%28C-GTSF%29_%40_LHR%2C_Aug_2009.jpg/500px-Air_Transat_A310_%28C-GTSF%29_%40_LHR%2C_Aug_2009.jpg",
         manufacturer: "Airbus",
         familyName: "A310 Family",
@@ -74,6 +77,7 @@ const aircraftData = [
     {
         id: "a318",
         photoVariant: "A318",
+        description: "A320 패밀리 중 가장 동체가 짧아 '베이비 버스(Baby Bus)'라는 별명을 가지고 있습니다. 런던 시티 공항 같이 활주로가 짧고 진입각이 가파른 공항에 이착륙할 수 있도록 특화 설계되었습니다.",
         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/F-GUGI_CDG_%2849806325246%29.jpg/500px-F-GUGI_CDG_%2849806325246%29.jpg",
         manufacturer: "Airbus",
         familyName: "A320 Family",
@@ -91,6 +95,7 @@ const aircraftData = [
     {
         id: "a319",
         photoVariant: "A319",
+        description: "A320을 약간 단축하여 항속거리를 늘린 모델입니다. 단거리 고수요 노선부터 중거리 노선까지 저비용 항공사(LCC)와 대형 항공사 모두에게 고루 인기를 끌었습니다.",
         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/United_Airbus_A319_%2813942617705%29.jpg/500px-United_Airbus_A319_%2813942617705%29.jpg",
         manufacturer: "Airbus",
         familyName: "A320 Family",
@@ -108,6 +113,7 @@ const aircraftData = [
     {
         id: "a320",
         photoVariant: "A320-200",
+        description: "에어버스의 초베스트셀러 협동체 기종으로, 여객기 최초로 디지털 플라이 바이 와이어(Fly-by-Wire) 조종 계통을 탑재하여 민항기 조종 역사의 새로운 패러다임을 연 항공기입니다.",
         imageUrl: "images/a320-200.jpg",
         manufacturer: "Airbus",
         familyName: "A320 Family",
@@ -125,6 +131,7 @@ const aircraftData = [
     {
         id: "a321",
         photoVariant: "A321-200",
+        description: "A320의 동체를 확장하여 좌석 공급량을 대폭 늘린 모델입니다. 협동체이면서도 높은 좌석당 경제성을 제공하여 전 세계 주요 고수요 단거리 노선의 핵심 기종으로 활약하고 있습니다.",
         imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Airbus_A321-231%28w%29_%E2%80%98N915US%E2%80%99_American_Airlines_%2828442733186%29.jpg/500px-Airbus_A321-231%28w%29_%E2%80%98N915US%E2%80%99_American_Airlines_%2828442733186%29.jpg",
         manufacturer: "Airbus",
         familyName: "A320 Family",
@@ -144,6 +151,7 @@ const aircraftData = [
     {
         id: "a319neo",
         photoVariant: "A319neo",
+        description: "A320neo 패밀리의 가장 작은 모델로, 최신 엔진과 샤클렛(Sharklet)을 장착하여 연료 효율을 극대화했습니다. 뛰어난 항속거리 성능으로 특수 중장거리 노선에 적합합니다.",
         imageUrl: "images/a319neo.jpeg",
         manufacturer: "Airbus",
         familyName: "A320neo Family",
@@ -161,6 +169,7 @@ const aircraftData = [
     {
         id: "a320neo",
         photoVariant: "A320neo",
+        description: "베스트셀러 A320에 차세대 엔진(New Engine Option)을 탑재하고 익단 날개인 샤클렛을 장착해 연료 소모와 탄소 배출을 20% 가까이 감축시킨 친환경 중단거리 항공기입니다.",
         imageUrl: "images/a320neo.jpg",
         manufacturer: "Airbus",
         familyName: "A320neo Family",
@@ -178,6 +187,7 @@ const aircraftData = [
     {
         id: "a321neo",
         photoVariant: "A321neo",
+        description: "A321에 신형 엔진을 적용하여 효율성을 극대화한 기종입니다. 항속거리를 대폭 늘린 A321LR 및 A321XLR 등의 파생형을 통해 협동체로 장거리 대서양 횡단 및 대륙 간 노선을 개척하고 있습니다.",
         imageUrl: "images/a321neo.jpg",
         manufacturer: "Airbus",
         familyName: "A320neo Family",
@@ -201,6 +211,7 @@ const aircraftData = [
     {
         id: "a330-family",
         photoVariant: "A330-300",
+        description: "에어버스의 중장거리 대표 쌍발 광동체 여객기입니다. 높은 신뢰성과 뛰어난 경제성 덕분에 전 세계 대형 항공사들의 장거리 노선 주력 항공기로 오랫동안 사랑받고 있습니다.",
         imageUrl: "images/a330-family.jpg",
         manufacturer: "Airbus",
         familyName: "A330 Family",
@@ -223,6 +234,7 @@ const aircraftData = [
     {
         id: "a330neo-family",
         photoVariant: "A330-900neo",
+        description: "베스트셀러 A330에 최신 엔진과 A350 스타일의 날개 기술을 접목한 최신 개량형 모델입니다. 운영 비용을 대폭 줄이면서도 승객 편의성(Airspace 객실)을 혁신적으로 개선했습니다.",
         imageUrl: "images/a330neo-family.webp",
         manufacturer: "Airbus",
         familyName: "A330neo Family",
@@ -245,6 +257,7 @@ const aircraftData = [
     {
         id: "a340-family",
         photoVariant: "A340-600",
+        description: "안정성을 극대화하기 위해 4발 엔진을 채택한 중장거리 여객기입니다. 쌍발기의 ETOPS 규제가 완화되기 전까지 초장거리 노선과 해양 비행의 최강자로 군림했던 기종입니다.",
         imageUrl: "images/a340-family.webp",
         manufacturer: "Airbus",
         familyName: "A340 Family",
@@ -269,6 +282,7 @@ const aircraftData = [
     {
         id: "a350-family",
         photoVariant: "A350-900",
+        description: "탄소 섬유 강화 플라스틱 등 첨단 신소재를 대거 사용하여 무게를 줄이고 연료 효율을 혁신적으로 높인 에어버스의 최첨단 광동체 플래그십 여객기입니다. 넓고 쾌적한 객실 환경을 제공합니다.",
         imageUrl: "images/a350-family.webp",
         manufacturer: "Airbus",
         familyName: "A350 Family",
@@ -292,6 +306,7 @@ const aircraftData = [
     {
         id: "a380",
         photoVariant: "A380-800",
+        description: "하늘 위의 5성급 호텔이라 불리는 세계 최대의 2층 복층형 광동체 여객기입니다. 압도적인 수송량과 조용하고 쾌적한 객실 공간으로 장거리 허브 공항 간 수송의 정점을 찍은 초대형 점보 기종입니다.",
         imageUrl: "images/a380.jpg",
         manufacturer: "Airbus",
         familyName: "A380",
@@ -311,6 +326,7 @@ const aircraftData = [
     {
         id: "atr-family",
         photoVariant: "ATR 72",
+        description: "프랑스와 이탈리아 합작사인 ATR에서 제작한 전 세계 대표 단거리 친환경 터보프롭(터빈 엔진 프롭기) 항공기입니다. 탁월한 연료 효율성과 거친 활주로에서도 운항 가능한 성능을 지녔습니다.",
         imageUrl: "images/tarom_skyteam.jpg",
         manufacturer: "Airbus",
         familyName: "ATR Series",
@@ -335,6 +351,7 @@ const aircraftData = [
     {
         id: "b707-family",
         photoVariant: "B707-320B",
+        description: "상업용 제트 여객기 시대를 본격적으로 개척한 보잉의 역사적인 4발 제트기입니다. 전 세계 민항기 여행의 대중화를 이끌었으며 현대 여객기의 기본 설계를 완성한 기념비적 모델입니다.",
         imageUrl: "images/northwest_707.jpg",
         manufacturer: "Boeing",
         familyName: "707 Family",
@@ -359,6 +376,7 @@ const aircraftData = [
     {
         id: "b717",
         photoVariant: "B717-200",
+        description: "보잉이 맥도넬 더글라스를 인수한 후 MD-95 설계를 이어받아 출시한 중단거리용 소형 쌍발 여객기입니다. 신뢰성이 높고 빠른 이착륙이 가능해 단거리 고빈도 노선에서 뛰어난 효율을 보여줍니다.",
         imageUrl: "images/b717.jpg",
         manufacturer: "Boeing",
         familyName: "717",
@@ -378,6 +396,7 @@ const aircraftData = [
     {
         id: "b727-family",
         photoVariant: "B727-200",
+        description: "동체 후방에 3개의 엔진을 배치하고 T자형 꼬리날개를 탑재한 보잉의 대표적인 3발 제트 여객기입니다. 우수한 활주로 이착륙 성능을 바탕으로 전 세계 중단거리 시장을 휩쓸었던 인기 모델입니다.",
         imageUrl: "images/b727-family.webp",
         manufacturer: "Boeing",
         familyName: "727 Family",
@@ -400,6 +419,7 @@ const aircraftData = [
     {
         id: "b737-original",
         photoVariant: "B737-200",
+        description: "현재 전 세계 협동체 시장의 양대 산맥인 보잉 737 시리즈의 시초가 된 기종입니다. 엔진이 날개 밑에 매우 가깝게 장착된 특유의 낮은 지상고 디자인이 특징입니다.",
         imageUrl: "images/b737-original.webp",
         manufacturer: "Boeing",
         familyName: "737 Original",
@@ -422,6 +442,7 @@ const aircraftData = [
     {
         id: "b737-classic",
         photoVariant: "B737-300",
+        description: "1세대 737에 이어 고효율 터보팬 엔진을 탑재하고 조종실 전자 장비를 개선한 2세대 737 시리즈입니다. 중단거리 수송량 증대에 큰 역할을 수행했습니다.",
         imageUrl: "images/b737-classic.jpg",
         manufacturer: "Boeing",
         familyName: "737 Classic",
@@ -445,6 +466,7 @@ const aircraftData = [
     {
         id: "b737-ng",
         photoVariant: "B737-800",
+        description: "Next Generation의 약자로, 날개 설계와 디지털 콕핏을 완전히 새롭게 하여 현대 단거리 여객기 시장의 베스트셀러가 된 3세대 737입니다. 뛰어난 안정성과 경제성을 자랑합니다.",
         imageUrl: "images/b737-ng.jpg",
         manufacturer: "Boeing",
         familyName: "737 NG",
@@ -469,6 +491,7 @@ const aircraftData = [
     {
         id: "b737-max",
         photoVariant: "B737 MAX 8",
+        description: "최신 CFMI LEAP-1B 엔진과 독특한 분할형 윙렛(Split-Scimitar Winglet)을 장착하여 연비 효율을 극대화한 4세대 737 시리즈입니다. 뛰어난 항속거리로 장거리 단거리 노선을 동시 커버합니다.",
         imageUrl: "images/737max8.jpg",
         manufacturer: "Boeing",
         familyName: "737 MAX",
@@ -493,6 +516,7 @@ const aircraftData = [
     {
         id: "b747-family",
         photoVariant: "B747-8I",
+        description: "'하늘의 여왕(Queen of the Skies)'으로 불리는 세계 최초의 복층형 초대형 광동체 여객기입니다. 반세기 넘게 장거리 민항 시장의 상징으로 군림하며 전 세계 대규모 장거리 해외 여행 대중화를 이끌었습니다.",
         imageUrl: "images/b747-family.jpg",
         manufacturer: "Boeing",
         familyName: "747 Family",
@@ -520,6 +544,7 @@ const aircraftData = [
     {
         id: "b757-family",
         photoVariant: "B757-200",
+        description: "단거리 고수요 노선과 중거리 대서양 횡단 노선을 동시에 커버하도록 설계된 단일 통로(협동체) 기종입니다. 뛰어난 추력 대비 중량 성능 덕분에 높은 고도의 공항이나 짧은 활주로에서도 이륙 성능이 매우 탁월합니다.",
         imageUrl: "images/b757-family.webp",
         manufacturer: "Boeing",
         familyName: "757 Family",
@@ -569,6 +594,7 @@ const aircraftData = [
     {
         id: "b777-family",
         photoVariant: "B777-300ER",
+        description: "'트리플 세븐'이라는 애칭으로 불리는 세계 최대의 쌍발 광동체 여객기입니다. 압도적인 크기의 엔진 추력과 광활한 기내 공간을 기반으로 3발 및 4발 광동체 여객기 시대를 종식시키고 장거리 표준 기종으로 자리매김했습니다.",
         imageUrl: "images/b777-family.jpg",
         manufacturer: "Boeing",
         familyName: "777 Family",
@@ -595,6 +621,7 @@ const aircraftData = [
     {
         id: "b777x-family",
         photoVariant: "B777-9",
+        description: "베스트셀러 777을 개량한 차세대 초대형 쌍발 여객기입니다. 공항 게이트 진입 시 날개 끝을 접을 수 있는 접이식 윙팁(Folding Wingtip)과 초경량 탄소섬유 날개를 적용해 혁신적인 연비 개선을 이뤄냈습니다.",
         imageUrl: "images/b777x-family.jpg",
         manufacturer: "Boeing",
         familyName: "777X Family",
@@ -617,6 +644,7 @@ const aircraftData = [
     {
         id: "b787-family",
         photoVariant: "B787-9",
+        description: "'드림라이너'라는 이름으로 친숙한 보잉의 혁신적인 친환경 중장거리 광동체 여객기입니다. 동체의 50% 이상에 탄소 복합 신소재를 적용하여 기압과 습도가 쾌적하게 유지되는 혁신적인 객실을 탑재했습니다.",
         imageUrl: "images/b787-family.jpg",
         manufacturer: "Boeing",
         familyName: "787 Dreamliner",
@@ -642,6 +670,7 @@ const aircraftData = [
     {
         id: "crj-family",
         photoVariant: "CRJ-700",
+        description: "캐나다 봄바디어(현재는 미쓰비시 항공)에서 개발한 전 세계 대표 중소형 지역 여객기(Regional Jet) 시리즈입니다. 소도시 간 빠른 연결 편을 제공하는 리전얼 노선에서 핵심 역할을 담당해 왔습니다.",
         imageUrl: "images/crj-family.jpeg",
         manufacturer: "Bombardier",
         familyName: "CRJ Series",
@@ -667,6 +696,7 @@ const aircraftData = [
     {
         id: "dash-8-family",
         photoVariant: "Dash 8-400",
+        description: "봄바디어사(현재는 De Havilland Canada)의 중단거리용 터보프롭 항공기입니다. 소음 및 진동 억제 기술(Q-Series)을 탑재하여 프롭기이면서도 매우 정숙하며, 우수한 이착륙 성능으로 섬이나 내륙 오지 노선에서 활약합니다.",
         imageUrl: "images/dash-8-family.jpg",
         manufacturer: "Bombardier",
         familyName: "Dash 8 Series",
@@ -693,6 +723,7 @@ const aircraftData = [
     {
         id: "erj-family",
         photoVariant: "ERJ-145XR",
+        description: "브라질의 엠브라에르사에서 제작한 소형 리전얼 제트 여객기 제품군입니다. 37~50인승 소규모 수송에 최적화되어 항공 여객 수요가 적은 중소도시 간 연결망 구축에 크게 기여했습니다.",
         imageUrl: "images/erj-family.jpg",
         manufacturer: "Embraer",
         familyName: "ERJ Family",
@@ -716,6 +747,7 @@ const aircraftData = [
     {
         id: "e-jet-family",
         photoVariant: "E175",
+        description: "엠브라에르의 대표적인 70~100인승급 대형 리전얼 제트기 패밀리입니다. 협동체 여객기에 버금가는 2-2 좌석 배치로 가운데 낀 좌석이 없으며 쾌적하고 넓은 기내 환경을 선사합니다.",
         imageUrl: "images/e-jet-family.webp",
         manufacturer: "Embraer",
         familyName: "E-Jet Family",
@@ -740,6 +772,7 @@ const aircraftData = [
     {
         id: "e-jet-e2-family",
         photoVariant: "E195-E2",
+        description: "성공적인 E-Jet 제품군에 프랫앤휘트니사의 차세대 기어드 터보팬(GTF) 엔진과 초정밀 날개 설계를 적용한 고효율 리전얼 제트기입니다. 동급 시장에서 가장 친환경적이고 조용한 운항 성능을 제공합니다.",
         imageUrl: "images/e-jet-e2-family.jpg",
         manufacturer: "Embraer",
         familyName: "E-Jet E2 Family",
@@ -765,6 +798,7 @@ const aircraftData = [
     {
         id: "md-11-family",
         photoVariant: "MD-11F",
+        description: "맥도넬 더글라스사의 상징적인 3발 광동체 여객기입니다. 꼬리날개 하단에 세 번째 엔진을 장착한 독특한 디자인이 특징이며, 여객기로서의 시대를 마감한 뒤 현재는 강력한 화물 수송력으로 화물기로 활약하고 있습니다.",
         imageUrl: "images/md-11-family.jpg",
         manufacturer: "McDonnell Douglas",
         familyName: "MD-11 Family",
@@ -787,6 +821,7 @@ const aircraftData = [
     {
         id: "md-80-family",
         photoVariant: "MD-90",
+        description: "MD-80 및 MD-90 시리즈는 동체 후방에 장착된 2개의 엔진과 날렵한 기체 형상이 특징인 협동체 여객기입니다. 높은 신뢰성과 기내 후방 특유의 저소음(전방 기준) 승차감으로 한 시대를 풍미했습니다.",
         imageUrl: "images/md-80-family.jpg",
         manufacturer: "McDonnell Douglas",
         familyName: "MD-80 / MD-90 Series",
